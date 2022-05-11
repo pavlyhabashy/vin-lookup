a39bc5ef9bac45064412cd34d8d4bd3334b02d59
Pavly Habashy <pavlyhabashy@gmail.com>
2022-05-11 14:59:25 -0700

    init vin lookup screen

---
+++ lib/screens/vin_lookup_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class VinLookupScreen extends StatelessWidget {
+  const VinLookupScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
