cd3bd8499dddd3990eec0ca58e10edca860fc765
Pavly Habashy <pavlyhabashy@gmail.com>
2022-05-11 18:46:44 -0700

    refactored vehicle and recall

--- lib/screens/vin_lookup_screen.dart
+++ lib/screens/vin_lookup_screen.dart
@@ -1,8 +1,12 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart';
+import 'package:string_validator/string_validator.dart';
+import 'package:vin_lookup/classes/recall.dart';
+import 'package:vin_lookup/classes/vehicle.dart';
 
 class VinLookupScreen extends StatefulWidget {
   const VinLookupScreen({Key? key}) : super(key: key);
@@ -15,56 +19,6 @@ class _VinLookupScreenState extends State<VinLookupScreen> {
   final _formKey = GlobalKey<FormState>();
   String _vin = "5J8TB1H29CA003675";
 
-  Future<Response> vinLookup(String vin) {
-    return get(
-      Uri.parse(
-          'https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/$vin?format=json'),
-    );
-  }
-
-  Future<Response> recallLookup(String make, String model, String year) {
-    return get(
-      Uri.parse(
-          'https://api.nhtsa.gov/recalls/recallsByVehicle?make=$make&model=$model&modelYear=$year'),
-    );
-  }
-
-  _submit() async {
-    if (_formKey.currentState!.validate()) {
-      _formKey.currentState!.save();
-
-      var response = await vinLookup(_vin);
-      var results = jsonDecode(response.body)["Results"] as List;
-      String make = "", model = "", year = "";
-      for (var result in results) {
-        switch (result["Variable"]) {
-          case "Make":
-            make = result["Value"].toString();
-            break;
-          case "Model":
-            model = result["Value"].toString();
-            break;
-          case "Model Year":
-            year = result["Value"].toString();
-            break;
-        }
-      }
-
-      print("$make, $model, $year");
-
-      var recallResponse = await recallLookup(make, model, year);
-      var recallResults = jsonDecode(recallResponse.body);
-
-      for (var result in recallResults["results"]) {
-        print(result["ReportReceivedDate"]);
-        print(result["Component"]);
-        print(result["Summary"]);
-        print(result["Consequence"]);
-        print(result["Remedy"]);
-      }
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -72,13 +26,24 @@ class _VinLookupScreenState extends State<VinLookupScreen> {
         child: Form(
           key: _formKey,
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               TextFormField(
                 initialValue: _vin,
                 decoration: const InputDecoration(labelText: 'Enter Your VIN'),
                 keyboardType: TextInputType.text,
                 autocorrect: false,
-                enableSuggestions: true,
+                enableSuggestions: false,
+                validator: (input) {
+                  if (input!.length != 17) {
+                    return 'Must be at least 17 characters';
+                  }
+                  if (!isAlphanumeric(input)) {
+                    return 'Must be letters and numbers';
+                  }
+                  return null;
+                },
                 inputFormatters: [
                   FilteringTextInputFormatter.deny(RegExp(r'\s')),
                 ],
@@ -87,7 +52,7 @@ class _VinLookupScreenState extends State<VinLookupScreen> {
               TextButton(
                 onPressed: _submit,
                 child: const Text(
-                  'Login',
+                  'Look Up',
                   style: TextStyle(
                     fontSize: 18.0,
                   ),
@@ -98,5 +63,84 @@ class _VinLookupScreenState extends State<VinLookupScreen> {
         ),
       ),
     );
+  }
+
+  Future<Response> vinGetRequest(String vin) {
+    return get(
+      Uri.parse(
+          'https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/$vin?format=json'),
+    );
+  }
+
+  Future<Response> recallGetRequest(String make, String model, String year) {
+    return get(
+      Uri.parse(
+          'https://api.nhtsa.gov/recalls/recallsByVehicle?make=$make&model=$model&modelYear=$year'),
+    );
+  }
+
+  _submit() async {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      if (!(await _checkConnection())) return;
+      await _lookUpVIN();
+    }
+  }
+
+  Future<bool> _checkConnection() async {
+    try {
+      final result = await InternetAddress.lookup('google.com');
+      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+        return true;
+      }
+    } on SocketException catch (_) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("No internet connection."),
+          behavior: SnackBarBehavior.floating,
+        ),
+      );
+      return false;
+    }
+    return false;
+  }
+
+  _lookUpVIN() async {
+    var response = await vinGetRequest(_vin);
+    var results = jsonDecode(response.body)["Results"] as List;
+    String make = "", model = "", year = "";
+    for (var result in results) {
+      switch (result["Variable"]) {
+        case "Make":
+          make = result["Value"].toString();
+          break;
+        case "Model":
+          model = result["Value"].toString();
+          break;
+        case "Model Year":
+          year = result["Value"].toString();
+          break;
+      }
+    }
+
+    if (make == "null" || model == "null" || year == "null") {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("VIN not found. Please check your VIN and try again."),
+          behavior: SnackBarBehavior.floating,
+        ),
+      );
+      return;
+    }
+
+    Vehicle vehicle = Vehicle(make: make, model: model, year: year);
+
+    var recallResponse = await recallGetRequest(make, model, year);
+    var recallResults = jsonDecode(recallResponse.body);
+    List<Recall> recalls = List.empty(growable: true);
+    for (var result in recallResults["results"]) {
+      recalls.add(Recall.fromJson(result));
+    }
+    vehicle.setRecalls(recalls);
   }
 }
