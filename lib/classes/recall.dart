b42c80f80dec33b8c45f34444c90c557ff82865b
Pavly Habashy <pavlyhabashy@gmail.com>
2022-05-11 18:11:06 -0700

    add vehicle and recall classes

---
+++ lib/classes/recall.dart
@@ -0,0 +1,22 @@
+class Recall {
+  final String reportReceivedDate;
+  final String component;
+  final String summary;
+  final String consequence;
+  final String remedy;
+
+  Recall(
+    this.reportReceivedDate,
+    this.component,
+    this.summary,
+    this.consequence,
+    this.remedy,
+  );
+
+  Recall.fromJson(Map<String, dynamic> json)
+      : reportReceivedDate = json['ReportReceivedDate'],
+        component = json['Component'],
+        summary = json["Summary"],
+        consequence = json["Consequence"],
+        remedy = json["Rememdy"];
+}
---
+++ lib/classes/vehicle.dart
@@ -0,0 +1,19 @@
+import 'package:vin_lookup/classes/recall.dart';
+
+class Vehicle {
+  final String make;
+  final String model;
+  final String year;
+  List<Recall>? recalls;
+
+  Vehicle({
+    required this.make,
+    required this.model,
+    required this.year,
+    this.recalls,
+  });
+
+  List<Recall>? getRecalls() {
+    return recalls;
+  }
+}
