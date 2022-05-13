ac1b6059eb4228b8ee84afeb9ef412582b0bf7b1
Pavly Habashy <pavlyhabashy@gmail.com>
2022-05-12 23:25:25 -0700

    future body instead of entire scaffold

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -12,64 +12,65 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<User>(
-      future: Authentication().pullUserAndReauthenticate(context),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          User user = snapshot.data!;
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text("Profile"),
-            ),
-            body: SingleChildScrollView(
-              child: SafeArea(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    children: [
-                      CircleAvatar(
-                        radius: 50,
-                        backgroundColor: Colors.green.shade50,
-                        child: const Icon(
-                          Icons.person,
-                          size: 50,
-                        ),
-                      ),
-                      const SizedBox(height: 16),
-                      Text(
-                        user.name,
-                        style: Theme.of(context).textTheme.headline5,
-                      ),
-                      const SizedBox(height: 48),
-                      _buildUserInfoRow(
-                        icon: Icons.email,
-                        title: "Email",
-                        info: user.email,
-                      ),
-                      _buildUserInfoRow(
-                        icon: Icons.phone_rounded,
-                        title: "Phone",
-                        info: user.phoneNumber,
-                      ),
-                      _buildUserInfoRow(
-                        icon: Icons.location_on,
-                        title: "Country",
-                        info: user.country,
-                      ),
-                    ],
-                  ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Profile"),
+      ),
+      body: FutureBuilder<User>(
+        future: Authentication().pullUserAndReauthenticate(context),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            User user = snapshot.data!;
+            return _buildBody(user, context);
+          }
+          return const Center(
+            child: CircularProgressIndicator.adaptive(),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildBody(User user, BuildContext context) {
+    return SingleChildScrollView(
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundColor: Colors.green.shade50,
+                child: const Icon(
+                  Icons.person,
+                  size: 50,
                 ),
               ),
-            ),
-          );
-        } else {
-          return const Scaffold(
-            body: Center(
-              child: CircularProgressIndicator.adaptive(),
-            ),
-          );
-        }
-      },
+              const SizedBox(height: 16),
+              Text(
+                user.name,
+                style: Theme.of(context).textTheme.headline5,
+              ),
+              const SizedBox(height: 48),
+              _buildUserInfoRow(
+                icon: Icons.email,
+                title: "Email",
+                info: user.email,
+              ),
+              _buildUserInfoRow(
+                icon: Icons.phone_rounded,
+                title: "Phone",
+                info: user.phoneNumber,
+              ),
+              _buildUserInfoRow(
+                icon: Icons.location_on,
+                title: "Country",
+                info: user.country,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 
@@ -88,7 +89,6 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 Icon(
                   icon,
                   color: Colors.green.shade700,
-                  size: Theme.of(context).iconTheme.size,
                 ),
                 const SizedBox(width: 8.0),
                 Text(title),
